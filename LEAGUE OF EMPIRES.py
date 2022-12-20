56c517f68fb23ec0757a78cf8a51a5b5a28b710c
Undziks <114875302+Undziks@users.noreply.github.com>
2022-12-20 11:42:42 +0000

    adding new level

--- LEAGUE OF EMPIRES.py
+++ LEAGUE OF EMPIRES.py
@@ -8,31 +8,49 @@ print(welcome_text)
 
 # Select nation
 print(70*'-')
-nations=('ROMAN', 'VIKING', 'EGIPTIAN', 'ZULUS', 'GERMAN', 'GREEK')
-print(f'Okay, so who do you choose? You want to be {nations}')
+nation_list=('ROMAN', 'VIKING', 'EGIPTIAN', 'ZULUS', 'GERMAN', 'GREEK')
+print(f'Okay, so who do you choose? You want to be {nation_list}')
 nation=input('Provide your nation: ').upper()
-while nation not in nations:
+while nation not in nation_list:
     nation=input(f'Sorry, I do not understand your input: {nation}, please enter again: ').upper()
 print(f'Great! You have choosen: {nation}')
 
-# Define player's name
+# Define player's name.
 print(70*'-')
 name=input('So, what\'s your name? Please enter your name: ')
 print(f'Welcome emperor: {name}')
 
-# Create first player's city
+# Create first player's city.
 print(70*'-')
 city=input('So, let\'s begin with first step, how do you call your first town?: ')
 print(f'New city: {city} is built by: {name}. ')
 
-# Build first building in the city
+# Build first building in the city.
 print(70*'-')
 print('You have done first step, now what do you want to build in your new town?')
-buildings=('BARACKS', 'HOUSES', 'TENTS', 'CASTLE')
-building=input(f'Provide your building which you want to build: {buildings} ').upper()
-while building not in buildings:
+building_list=('BARACKS', 'HOUSES', 'TENTS', 'CASTLE')
+
+building=input(f'Provide your building which you want to build: {building_list} ').upper()
+while building not in building_list:
     building=input(f'You can\'t build this building: {building}').upper()
 print(f'Great! Emperor {name} built: {building} in city: {city}')
-    
+
 print(70*'-')
+#Choose people to work for you.
+people_list=('MILITARY', 'NORMAL', 'FARMERS', 'BUILDERS')
+people=input(f'Do you want to have more miltary people, or normal people? You can choose: {people_list}.').upper()
+
+while people not in people_list:
+    people=input(f'This people are not yet unlocked plase try again').upper()
+print('Very good, now you have 110 people in your city')
+
+print(70*'-')
+#You have finished LVL1.
+print('Great! You have finished LVL1. Now you are on the LVL2.')
+print(70*'-')
+
+
+
+
+
 
